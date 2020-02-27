971dec9612610244822f84a96c4114af8ddc650f
Rizwan Qureshi <rizwan@ualberta.ca>
2020-02-26 23:15:10 -0700

    connecting settings UI to fragment class

--- app/src/main/java/com/ece493/group5/adjustableaudio/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/ece493/group5/adjustableaudio/ui/settings/SettingsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,18 +20,116 @@ public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
 
+    private Spinner presetSpinner;
+
+    private SeekBar[] equalizerSeekbars;
+    private TextView[] equalizerValues;
+
+    private SeekBar globalVolumeSeekbar;
+    private TextView globalVolumeValue;
+
+    private SeekBar leftVolumeSeekbar;
+    private TextView leftVolumeValue;
+
+    private SeekBar rightVolumeSeekbar;
+    private TextView rightVolumeValue;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-//        final TextView textView = root.findViewById(R.id.text_settings);
+
+        presetSpinner = root.findViewById(R.id.presetSpinner);
+
+        equalizerSeekbars = new SeekBar[] {
+                root.findViewById(R.id.equalizerBandSeekbar1),
+                root.findViewById(R.id.equalizerBandSeekbar2),
+                root.findViewById(R.id.equalizerBandSeekbar3),
+                root.findViewById(R.id.equalizerBandSeekbar4),
+                root.findViewById(R.id.equalizerBandSeekbar5)
+        };
+        equalizerValues = new TextView[] {
+                root.findViewById(R.id.equalizerBandValue1),
+                root.findViewById(R.id.equalizerBandValue1),
+                root.findViewById(R.id.equalizerBandValue1),
+                root.findViewById(R.id.equalizerBandValue1),
+                root.findViewById(R.id.equalizerBandValue1)
+        };
+
+        globalVolumeSeekbar = root.findViewById(R.id.settingsGlobalVolumeSeekbar);
+        globalVolumeValue = root.findViewById(R.id.settingsGlobalVolumeValue);
+
+        leftVolumeSeekbar = root.findViewById(R.id.settingsLeftVolumeSeekbar);
+        leftVolumeValue = root.findViewById(R.id.settingsLeftVolumeValue);
+
+        rightVolumeSeekbar = root.findViewById(R.id.settingsRightVolumeSeekbar);
+        rightVolumeValue = root.findViewById(R.id.settingsRightVolumeValue);
+
+        for (int i = 0; i < equalizerSeekbars.length; i++) {
+            final TextView textView = equalizerValues[i];
+            equalizerSeekbars[i].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    textView.setText(String.valueOf(progress));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+        }
+
+        globalVolumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                globalVolumeValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        leftVolumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                leftVolumeValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        rightVolumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rightVolumeValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+
 //        settingsViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
 //            }
 //        });
+
         return root;
     }
 }
