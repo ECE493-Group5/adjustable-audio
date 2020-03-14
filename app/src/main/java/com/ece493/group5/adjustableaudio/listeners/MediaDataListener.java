35699babb2b004aecfe94e61cdcf35117e7f75b3
Rizwan Qureshi <rizwan@ualberta.ca>
2020-03-14 15:32:24 -0600

    Added a way to control audio balancing of the MediaPlayer

--- app/src/main/java/com/ece493/group5/adjustableaudio/listeners/MediaDataListener.java
+++ app/src/main/java/com/ece493/group5/adjustableaudio/listeners/MediaDataListener.java
@@ -2,49 +2,43 @@ package com.ece493.group5.adjustableaudio.listeners;
 
 import android.os.Bundle;
 
-import com.ece493.group5.adjustableaudio.adapters.MediaPlayerAdapter;
 import com.ece493.group5.adjustableaudio.models.MediaData;
 import com.ece493.group5.adjustableaudio.models.Song;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class MediaDataListener
 {
-    public static final String EXTRA_STATE =  "BUNDLE_STATE";
-    public static final String EXTRA_QUEUE =  "BUNDLE_QUEUE";
-    public static final String EXTRA_QUEUE_INDEX =  "BUNDLE_QUEUE_INDEX";
-    public static final String EXTRA_SONG =  "BUNDLE_SONG";
-    public static final String EXTRA_TOTAL_DURATION = "BUNDLE_TOTAL_DURATION";
-    public static final String EXTRA_ELAPSED_DURATION = "BUNDLE_ELAPSED_DURATION";
-
-    public void handleChange(Bundle extras)
+    public void handleChanges(Bundle extras)
     {
-        MediaData mediaData = MediaData.extract(extras);
+        handleChanges(MediaData.extract(extras));
+    }
 
+    public void handleChanges(MediaData mediaData)
+    {
         if (mediaData.queueChanged())
-        {
             onQueueChanged(mediaData.getQueue());
-        }
 
         if (mediaData.queueIndexChanged())
-        {
             onQueueIndexChanged(mediaData.getQueueIndex(), mediaData.getCurrentSong());
-        }
 
         if (mediaData.stateChanged())
-        {
             onStateChanged(mediaData.getState());
-        }
 
         if (mediaData.durationChanged())
-        {
             onDurationChanged((int) mediaData.getElapsedDuration(), (int) mediaData.getTotalDuration());
-        }
+
+        if (mediaData.leftVolumeChanged())
+            onLeftVolumeChanged(mediaData.getLeftVolume());
+
+        if (mediaData.rightVolumeChanged())
+            onRightVolumeChanged(mediaData.getRightVolume());
     }
 
     public void onQueueChanged(List<Song> queue) {}
     public void onQueueIndexChanged(int index, Song song) {}
     public void onStateChanged(int state) {}
     public void onDurationChanged(int elapsed, int total) {}
+    public void onLeftVolumeChanged(double percent) {}
+    public void onRightVolumeChanged(double percent) {}
 }
