bbb9765fa7e3ab515124502bec807d9f588b4c35
atyrrell1 <tyrrell1@ualberta.ca>
2020-03-28 19:16:32 -0600

    Added Splash Screen

---
+++ app/src/main/java/com/ece493/group5/adjustableaudio/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.ece493.group5.adjustableaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent openMainActivity =  new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(openMainActivity);
+                finish();
+
+            }
+        }, SPLASH_TIME);
+    }
+}
