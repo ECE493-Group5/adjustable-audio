d1e0392cd822d2a6aecf690beefbf1f5f36fc16c
atyrrell1 <tyrrell1@ualberta.ca>
2020-03-29 11:52:37 -0600

    Added Disclaimer

--- app/src/main/java/com/ece493/group5/adjustableaudio/SplashActivity.java
+++ app/src/main/java/com/ece493/group5/adjustableaudio/SplashActivity.java
@@ -2,13 +2,18 @@ package com.ece493.group5.adjustableaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.TextView;
+import android.preference.PreferenceManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity
+{
+    private static final String SHARED_PREFS_FILE = "DATATYPE_STORAGE";
+    private static final String DISCLAIMER_KEY = "DISCLAIMER";
+
     private int SPLASH_TIME = 3000;
 
     @Override
@@ -16,13 +21,27 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        SharedPreferences prefs = this.getSharedPreferences(SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+        final boolean disclaimerUnderstood = prefs.getBoolean(DISCLAIMER_KEY, false);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run()
             {
-                Intent openMainActivity =  new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(openMainActivity);
+                Intent openNextActivity;
+
+                if (disclaimerUnderstood)
+                {
+                    openNextActivity =  new Intent(SplashActivity.this,
+                            MainActivity.class);
+                }
+                else
+                {
+                    openNextActivity = new Intent(SplashActivity.this,
+                            DisclaimerActivity.class);
+                }
+                startActivity(openNextActivity);
                 finish();
             }
         }, SPLASH_TIME);
