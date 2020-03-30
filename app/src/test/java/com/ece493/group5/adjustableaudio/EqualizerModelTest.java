f3f011a4f5adb20178e9ac535d7cbc9c0187b944
atyrrell1 <tyrrell1@ualberta.ca>
2020-03-30 15:12:51 -0600

    EqualizerPresetUnitTest

---
+++ app/src/test/java/com/ece493/group5/adjustableaudio/EqualizerModelTest.java
@@ -0,0 +1,16 @@
+package com.ece493.group5.adjustableaudio;
+
+import com.ece493.group5.adjustableaudio.models.EqualizerModel;
+
+import org.junit.Before;
+
+public class EqualizerModelTest
+{
+    EqualizerModel testEqualizerModel;
+
+    @Before
+    public void equalizerModelTestSetup()
+    {
+    }
+
+}
