c244fd71038c26fcea37d395fd087afce8c00ce2
szuk1 <szuk@ualberta.ca>
2020-03-31 20:52:11 -0600

    Adding ear labels to hearing test and bugfixes

--- app/src/main/java/com/ece493/group5/adjustableaudio/ui/hearing_test/HearingTestFragment.java
+++ app/src/main/java/com/ece493/group5/adjustableaudio/ui/hearing_test/HearingTestFragment.java
@@ -2,6 +2,7 @@ package com.ece493.group5.adjustableaudio.ui.hearing_test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,7 +72,16 @@ public class HearingTestFragment extends Fragment {
             }
         });
 
+        Log.d("HearingTestFragment", "OnCreateView Called");
         return root;
+    }
+
+    @Override
+    public void onResume()
+    {
+        Log.d("HearingTestFragment", "OnResume Called");
+        testResultListAdapter.notifyDataSetChanged();
+        super.onResume();
     }
 
     private void switchFragment(int testResultPosition)
