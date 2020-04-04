086edcbd1c8ef06260c7e26d78b134ed26bb7465
szuk1 <szuk@ualberta.ca>
2020-04-04 00:33:26 -0600

    MicrophoneData tests

--- app/src/test/java/com/ece493/group5/adjustableaudio/MicrophoneDataUnitTest.java
+++ app/src/test/java/com/ece493/group5/adjustableaudio/MicrophoneDataUnitTest.java
@@ -5,12 +5,74 @@ import com.ece493.group5.adjustableaudio.models.MicrophoneData;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
 public class MicrophoneDataUnitTest
 {
+
+    public static final int MODE_NORMAL = 1;
+    public static final int MODE_NOISE_SUPPRESSION = 2;
+    public static final int MODE_SPEECH_FOCUS = 3;
+
     @Test
     public void microphoneDataConstructorTest()
     {
         MicrophoneData microphoneData = new MicrophoneData();
+
+        assertFalse(microphoneData.getIsRecording());
+
+        assertEquals(MODE_NORMAL, microphoneData.getMode());
+    }
+
+    @Test
+    public void setModeTest()
+    {
+        MicrophoneData microphoneData = new MicrophoneData();
+
+        assertNotEquals(MODE_NOISE_SUPPRESSION, microphoneData.getMode());
+
+        microphoneData.setMode(MODE_NOISE_SUPPRESSION);
+
+        assertEquals(MODE_NOISE_SUPPRESSION, microphoneData.getMode());
+    }
+
+    @Test
+    public void setIsRecordingTest()
+    {
+        MicrophoneData microphoneData = new MicrophoneData();
+
+        assertFalse(microphoneData.getIsRecording());
+
+        microphoneData.setIsRecording(true);
+
+        assertTrue(microphoneData.getIsRecording());
+    }
+
+    @Test
+    public void isRecordingChangedTest()
+    {
+        MicrophoneData microphoneData = new MicrophoneData();
+
+        assertFalse(microphoneData.isRecordingChanged());
+
+        microphoneData.setIsRecording(true);
+
+        assertTrue(microphoneData.isRecordingChanged());
+    }
+
+    @Test
+    public void modeChangedTest()
+    {
+        MicrophoneData microphoneData = new MicrophoneData();
+
+        assertFalse(microphoneData.modeChanged());
+
+        microphoneData.setMode(MODE_SPEECH_FOCUS);
+
+        assertTrue(microphoneData.modeChanged());
     }
 
 }
